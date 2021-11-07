d0cad8325eb682014ccaa4a9b97aaf7e3046120d
elthomaso <GueutalThomas@outlook.com>
2021-11-07 14:37:26 +0100

    - moved files into folders in src/ - removed boilerplate comments from main.py

---
+++ src/Vigenere/Vigenere.py
@@ -0,0 +1,6 @@
+"""
+kt = kolom transpositie
+v = vigenere
+m = message
+(  kt(v(m))  )⁻¹  =  (kt ° v)⁻¹(m) = (v⁻¹ ° kt⁻¹)(m) = v⁻¹(kt⁻¹(m))
+"""
---
+++ src/Vigenere/VigenerePlus.py
@@ -0,0 +1,7 @@
+"""
+kt = kolom transpositie
+v = vigenere
+m = message
+(  kt(v(m))  )⁻¹  =  (kt ° v)⁻¹(m) = (v⁻¹ ° kt⁻¹)(m) = v⁻¹(kt⁻¹(m))
+"""
+

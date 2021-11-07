cf967acc547e73d2342f36dde6996973de4e9e81
elthomaso <GueutalThomas@outlook.com>
2021-11-07 15:18:29 +0100

    Changes by mr_kroket

--- src/Vigenere/Vigenere.py
+++ src/Vigenere/Vigenere.py
@@ -1,3 +1,13 @@
 """
 This file implements the vigenere and column transposition encryption techniques.
 """
+
+
+def vigenere(msg: str, key: str) -> str:
+    transformedMsg: str = ""
+
+
+
+    return transformedMsg
+
+def

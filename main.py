88442b5dfef836daa6eb95639940e4fa4c522710
elthomaso <GueutalThomas@outlook.com>
2021-11-07 18:18:33 +0100

    - added column transposition

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-from src.Vigenere.Vigenere import vigenere
+from src.Vigenere.Vigenere import vigenere, columnTransposition
+
 
 
 if __name__ == '__main__':
-    msg = "Hello World!"
-    key = "body"
-    vigenere(msg, key)
-
+    pass
--- src/Vigenere/Vigenere.py
+++ src/Vigenere/Vigenere.py
@@ -10,4 +10,29 @@ def vigenere(msg: str, key: str) -> str:
 
     return transformedMsg
 
-def
+
+def columnTransposition(msg: str, key: str) -> str:
+    transposed = ""
+
+    msg = msg.lower()
+    key = key.lower()
+    s = sorted(key)
+    m: dict = {c: v for c, v in enumerate(s)}
+    columns = [(-1, -1)] * len(key)   # (sorted key index, unsorted key index)
+
+    for pair in m.items():
+        for index in range(len(key)):
+            if key[index] == pair[1] and columns[index][0] == -1:
+                columns[index] = (pair[0], index)
+                break
+
+    # sort the columns based on assigned numerical value (sort a-z)
+    columns = sorted(columns, key=lambda pair: pair[0])
+
+    for col in columns:
+        msgInd = col[1]
+        while msgInd < len(msg):
+            transposed += msg[msgInd]
+            msgInd += len(key)
+
+    return transposed

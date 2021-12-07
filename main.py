d4130486dbeccf68544e3c3cd098705fc89df4a9
elthomaso <GueutalThomas@outlook.com>
2021-12-07 22:54:28 +0100

    - edited Key26B incr() and decr() to take an index value. You can now choose a specific index to increment or decrement, e.g.     key = 'AZZCAA'     key.incr(1)       ==>  key == 'AAADAAA'     key.decr(4)  [underflow]       ==>  key == 'AAADZZZ'     key.incr(4)  [overflow]       ==>  key == 'AAADAAA' - clean main

--- main.py
+++ main.py
@@ -14,8 +14,23 @@ from src.Utils.Utils import *
 
 
 if __name__ == '__main__':
-    pass
 
+    key = KeyB26(7, 0 + 25*26 + 25*26*26 + 2*26*26*26)
+    print(key.key())
+    print(key.seed())
+    print()
 
+    key.incr(1)
+    print(key.key())
+    print(key.seed())
+    print()
 
+    key.decr(4)
+    print(key.key())
+    print(key.seed())
+    print()
 
+    key.incr(4)
+    print(key.key())
+    print(key.seed())
+    print()
